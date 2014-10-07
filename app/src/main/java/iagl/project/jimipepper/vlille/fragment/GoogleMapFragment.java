d63a83d74359f5039eae1869a32dec4bd0c7e1ae
jimipepper <maitre.jiraya@live.fr>
2014-10-07 02:03:36 +0200

    Suppression d'un import inutile

--- app/src/main/java/iagl/project/jimipepper/vlille/fragment/GoogleMapFragment.java
+++ app/src/main/java/iagl/project/jimipepper/vlille/fragment/GoogleMapFragment.java
@@ -10,7 +10,6 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.List;
